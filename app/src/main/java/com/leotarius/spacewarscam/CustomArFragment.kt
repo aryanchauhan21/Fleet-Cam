dec3098a7bb0cbcf2f8530c860d4e2761020f337
Aryan Chauhan <sansarya2121@gmail.com>
2020-07-19 23:44:46 +0530

    Added custom ARfragment

---
+++ app/src/main/java/com/leotarius/spacewarscam/CustomArFragment.kt
@@ -0,0 +1,25 @@
+package com.leotarius.spacewarscam
+
+import android.os.Bundle
+import android.view.View
+import com.google.ar.sceneform.ux.ArFragment
+
+class CustomArFragment: ArFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+        arSceneView.planeRenderer.isEnabled = false
+    }
+
+    override fun getAdditionalPermissions(): Array<String> {
+        val additionalPermissions = super.getAdditionalPermissions()
+        val permissionLength = additionalPermissions.size
+        val newPermissions = Array(permissionLength+1) { android.Manifest.permission.WRITE_EXTERNAL_STORAGE }
+        if(permissionLength>0){
+            System.arraycopy(additionalPermissions, 0, newPermissions, 1, permissionLength )
+        }
+        return newPermissions
+    }
+}
