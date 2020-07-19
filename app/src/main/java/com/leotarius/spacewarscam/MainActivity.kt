bf31a1f0f62d7e933bd7fdd200d3f0a393eaa56c
Aryan Chauhan <sansarya2121@gmail.com>
2020-07-19 06:16:40 +0530

    arcore setup and Initial UI setup

---
+++ app/src/main/java/com/leotarius/spacewarscam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leotarius.spacewarscam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
