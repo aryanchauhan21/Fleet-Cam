5dac2a35188a8fb159f23d8d99f9994e35d35110
Aryan Chauhan <sansarya2121@gmail.com>
2020-07-19 22:27:06 +0530

    some minor tweaks

--- app/src/main/java/com/leotarius/spacewarscam/MainActivity.kt
+++ app/src/main/java/com/leotarius/spacewarscam/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.leotarius.spacewarscam
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.VisibilitySetterAction
 import android.util.Log
 import android.util.TypedValue
+import android.util.Xml
 import android.view.View
 import android.widget.Toast
 import androidx.core.view.isVisible
@@ -24,7 +27,9 @@ class MainActivity : AppCompatActivity() {
     private var currCamPosition = Vector3.zero()
     lateinit var centreNode: Node
     lateinit var arFragment: ArFragment
-
+    var distanceFromCenter: Float = 0f
+    var heightAboveGround: Float = 0f
+    var rotationSpeeed: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,8 +49,19 @@ class MainActivity : AppCompatActivity() {
         }
         
         launch.setOnClickListener {
+
+            // Disabling th button until the model is fully loaded.
+            launch.isEnabled = false
+            launch.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#f6e58d"));
+
+            // Taking values from slider
+            distanceFromCenter = radius.value
+            heightAboveGround = height.value
+            rotationSpeeed = speed.value
+
+            // loading the model and adding to scene
             loadModelAndAddToScene(R.raw.star_destroyer)
-            launch.isEnabled = false;
+
             Log.d(TAG, "onCreate: adding to scene")
         }
 
@@ -62,26 +78,33 @@ class MainActivity : AppCompatActivity() {
             .setSource(this, modelResourceId)
             .build()
             .thenAccept {
-                addNodeToScene(it)
+                val warship = when(modelResourceId){
+                    R.raw.star_destroyer -> Warship.StarDestroyer
+                    R.raw.tie_silencer -> Warship.TieSilencer
+                    R.raw.xwing -> Warship.XWing
+                    else -> Warship.XWing
+                }
+                addNodeToScene(it, warship)
             }.exceptionally {
                 Toast.makeText(this, "Error creating node ", Toast.LENGTH_SHORT).show()
                 null
             }
     }
 
-    private fun addNodeToScene(modelRenderable: ModelRenderable?) {
+    private fun addNodeToScene(modelRenderable: ModelRenderable?, warship: Warship) {
         Log.d(TAG, "addNodeToScene: adding a node to the scene")
 
-        val rotatingNode = RotatingNode(90f).apply {
+        val rotatingNode = RotatingNode(rotationSpeeed).apply {
             setParent(centreNode)
         }
         Node().apply {
             renderable = modelRenderable
             setParent(rotatingNode)
-            localPosition = Vector3(2f, 1f, 0f)
-            localRotation = Quaternion.eulerAngles(Vector3(0f, 180f, 0f))
+            localPosition = Vector3(distanceFromCenter, heightAboveGround, 0f)
+            localRotation = Quaternion.eulerAngles(Vector3(0f, warship.rotationDegree, 0f))
         }
         Log.d(TAG, "addNodeToScene: added to scene")
+        launch.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#ffdd59"));
         launch.isEnabled = true
     }
 
