bcc42d6b3e78c59aa613e0bd806ff8620c2a9584
Aryan Chauhan <sansarya2121@gmail.com>
2020-07-27 03:41:50 +0530

    made changes to comply with play store policy... added credits , changed icons and assets

---
+++ app/src/main/java/com/leotarius/FleetCam/warship.kt
@@ -0,0 +1,20 @@
+package com.leotarius.FleetCam
+
+sealed class Warship {
+    abstract val rotationDegree: Float
+
+    object StarDestroyer: Warship(){
+        override val rotationDegree: Float
+            get() = 180f
+    }
+
+    object TieSilencer: Warship(){
+        override val rotationDegree: Float
+            get() = 90f
+    }
+
+    object XWing: Warship(){
+        override val rotationDegree: Float
+            get() = -90f
+    }
+}
